d3d7328b46c2733f380153a28a82704fc1ca3beb
GracieChristine <graciechristineli@gmail.com>
2017-10-13 10:50:02 -0600

    Deploying to heroku.

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let express = require('express')
+let path = require('path')
+let serveStatic = require('serve-static')
+
+let app = express()
+app.use(serveStatic(__dirname + '/dist'))
+
+let port = process.env.PORT || 5000
+app.listen(port);
+console.log('This vue\'s server started at ....' + port);
