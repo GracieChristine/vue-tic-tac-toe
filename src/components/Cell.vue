a0b52184a04f249b1ef5aabf45eccac7454828f6
GracieChristine <graciechristineli@gmail.com>
2017-10-13 09:39:30 -0600

    Alternating X and O.

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,5 +1,5 @@
 <template>
-<td class="cell">{{ mark }}</td>
+<td class="cell" @click="strike">{{ mark }}</td>
 </template>
 
 <script>
@@ -12,6 +12,19 @@ export default {
 
       // holds either X or O to be displayed in the td
       mark: ''
+    }
+  },
+  methods: {
+    strike() {
+      if (! this.frozen) {
+        // gets either X or O from the Grid component
+        this.mark = this.$parent.activePlayer
+
+        this.frozen = true
+
+        // fires a evet to notify the Grid component that a mark is placed
+        Event.$emit('strike', this.name)
+      }
     }
   }
 }
--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -19,11 +19,12 @@
 </template>
 
 <script>
-
 import Cell from './Cell.vue'
 
 export default {
-  components: { Cell },
+  components: {
+    Cell
+  },
   data() {
     return {
       // can be O or X
@@ -35,25 +36,112 @@ export default {
       // status color is used as background color in the status bar
       // it can hold the name of either of the following CSS classes
       // statusTurn (default) is yellow for a turn
-        // statusWin is green for a win
-        // statusDraw is purple for a draw
+      // statusWin is green for a win
+      // statusDraw is purple for a draw
 
       gameStatusColor: 'statusTurn',
       // # of moves played by both players in a single game (max = 9)
       moves: 0,
       // stores the placement of X and O in cells by their cell number
       cells: {
-        1: '', 2: '', 3: '',
-        4: '', 5: '', 6: '',
-        7: '', 8: '', 9: '',
+        1: '',
+        2: '',
+        3: '',
+        4: '',
+        5: '',
+        6: '',
+        7: '',
+        8: '',
+        9: '',
       },
       // contains all (8) possible winning conditions
       winConditions: [
-        [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
-        [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
-        [1, 5, 9], [3, 5, 7]             // diagonals
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9], // rows
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9], // columns
+        [1, 5, 9],
+        [3, 5, 7] // diagonals
       ],
     }
+  },
+  computed: {
+    // helper property to get the non-active player
+    nonActivePlayer() {
+      if (this.activePlayer === 'O') {
+        return 'X'
+      }
+      return 'O'
+    }
+  },
+  methods: {
+    // changes the active player to the non-active player with the help of the nonActivePlayer computed property
+    changePlayer() {
+      this.activePlayer = this.nonActivePlayer
+    },
+    // checks for possible win conditions from the data
+    checkForWin() {
+      for (let i = 0; i < this.winConditions.length; i++) {
+        // gets a single condition wc from the whole array
+        let wc = this.winConditions[i]
+        let cells = this.cells
+        // compares 3 cell values based on the cells in the condition
+        if (this.areEqual(cells[wc[0]], cells[wc[1]], cells[wc[2]])) {
+          return true
+        }
+      }
+      return false
+    },
+    gameIsWon() {
+      // fires win event for the App component to change the score
+      Event.$emit('win', this.activePlayer)
+      // sets the game status message
+      this.gameStatusMessage = `${this.activePlayer} Wins !`
+      // fires an event for the Cell to freeze
+      Event.$emit('freeze')
+      // sets the status to win
+      return 'win'
+    },
+    // returns the game status to the gameStatus property
+    changeGameStatus() {
+      if (this.checkForWin()) {
+        return this.gameIsWon()
+        // checks if the game is still not won and all cells are filled
+      } else if (this.moves === 9) {
+        // sets the status to draw
+        return 'draw'
+      }
+      // sets the status to turn
+      return 'turn'
+    },
+    // helper function for comparing cell values
+    areEqual() {
+      var len = arguments.length;
+      // loops through each value and compares them with an empty sting and
+      // for inequality
+      for (var i = 1; i < len; i++) {
+        if (arguments[i] === '' || arguments[i] !== arguments[i - 1])
+          return false;
+      }
+      return true;
+    }
+  },
+  // listening for strike made by user on cell, from the Cell component
+  created() {
+    Event.$on('strike', (cellNumber) => {
+      // sets either X or O in the clicked cell of the cells array
+      this.cells[cellNumber] = this.activePlayer
+
+      // increments the # of moves
+      this.moves++
+
+        // stores the game status by calling the changeGameStatus method
+        this.gameStatus = this.changeGameStatus()
+
+      this.changePlayer()
+    })
   }
 }
 </script>
